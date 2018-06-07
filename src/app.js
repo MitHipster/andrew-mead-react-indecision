5f074d96ad3d803a87bb98cfcbfcf3dcea3a431b
Tim Acker <tjacker31@gmail.com>
2018-06-06 21:52:27 -0400

    Props
    Passed initial props to components.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 class IndecisionApp extends React.Component {
 	render() {
+		const title = 'Indecision';
+		const subtitle = 'Put your life in the hands of a computer';
+		const options = ['Thing one', 'Thing two', 'Thing four'];
+
 		return (
 			<div>
-				<h1>Title</h1>
-				<Header />
+				<Header title={title} subtitle={subtitle} />
 				<Action />
-				<Options />
+				<Options options={options} />
 				<AddOption />
 			</div>
 		);
@@ -16,8 +19,8 @@ class Header extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Indecision</h1>
-				<h2>Put your life in the hands of a computer</h2>
+				<h1>{this.props.title}</h1>
+				<h2>{this.props.subtitle}</h2>
 			</div>
 		);
 	}
@@ -37,8 +40,7 @@ class Options extends React.Component {
 	render() {
 		return (
 			<div>
-				Options go here
-				<Option />
+				{this.props.options.map((option, i) => <Option key={i} text={option} />)}
 			</div>
 		);
 	}
@@ -46,7 +48,11 @@ class Options extends React.Component {
 
 class Option extends React.Component {
 	render() {
-		return <div>Option 1</div>;
+		return (
+			<div>
+				{this.props.text}
+			</div>
+		);
 	}
 }
 
