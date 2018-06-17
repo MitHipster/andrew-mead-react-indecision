4fcc0ca615b90f6bc492fa4bea52c011c8096c75
Tim Acker <tjacker31@gmail.com>
2018-06-16 20:01:06 -0400

    Modal Styling
    Styled third-party modal component.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -8,10 +8,12 @@ const OptionModal = props => (
 		// Closes modal with the ESC key or when clicking outside of the modal
 		onRequestClose={props.handleClearSelectedOption}
 		contentLabel="Selected Option"
+		closeTimeoutMS={200}
+		className="modal"
 	>
-		<h3>Selected Option</h3>
-		{props.selectedOption && <p>{props.selectedOption}</p>}
-		<button onClick={props.handleClearSelectedOption}>OK</button>
+		<h3 className="modal__title">Selected Option</h3>
+		{props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+		<button className="sm-button" onClick={props.handleClearSelectedOption}>OK</button>
 	</Modal>
 );
 
